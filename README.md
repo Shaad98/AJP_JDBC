823e364c76bf291b26c1fcf65a02dd5e0fb524d0
Shaad <141047163+Shaad98@users.noreply.github.com>
2024-09-29 09:55:15 +0530

    All Operations are Performed On Statement Interface

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# AJP_JDBC
+# AJP_JDBC  
 JDBC important concepts are covered.
+
+1. Execute Methods
+    - execute()
+        - Return Booolean Value . Execute all types of operation . Return true when it get **ResultSet**
+    - executeUpdate()
+        - Return Integer Value. Execute all operations . But useful in Insert, Update and Delete
+    - executeQuery()
+        - Return ResultSet . Execute All Operations. It gives exception when query doesn't return ResultSet.
+        
--- demo/src/main/java/com/example/App.java
+++ demo/src/main/java/com/example/App.java
@@ -13,11 +13,30 @@ public class App
         String url = "jdbc:postgresql://localhost:5432/practice";
         String username = "postgres";
         String password = "Shaad#98";
-        System.out.println( "Hello World!" );
+        String insert = "INSERT INTO student (id,name) VALUES (1,'Ravi')";
+        String update = "UPDATE student SET name='Pavan' WHERE id = 2";
+        String delete = "DELETE FROM student WHERE id = 3";
+        String select = "SELECT * FROM student";
+        // System.out.println( "Hello World!" );
         try{
             Class.forName("org.postgresql.Driver");
             Connection connection = DriverManager.getConnection(url, username, password);
-            System.out.println("Connection is Successful!");
+            Statement st = connection.createStatement();
+            int r = st.executeUpdate(insert);
+            System.out.println(r+" row inserted!");
+
+            r = st.executeUpdate(update);
+            System.out.println(r+" row updated!");
+
+            r = st.executeUpdate(delete);
+            System.out.println(r+" row deleted!");
+
+            ResultSet rs = st.executeQuery(select);
+            while(rs.next())
+            {
+                System.out.println(rs.getInt(1)+" : "+rs.getString(2));
+            }
+            System.out.println("All operations are successfully performed!");
             connection.close();
         }catch(Exception e)
         {
