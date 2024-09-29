652e07c4fd2f45afb1e164718e7fb00a678791eb
Shaad <141047163+Shaad98@users.noreply.github.com>
2024-09-29 09:02:22 +0530

    Connection is Successful Without Driver Name

---
+++ demo/src/main/java/com/example/App.java
@@ -0,0 +1,26 @@
+package com.example;
+import java.sql.*;
+/**
+ * Hello world!
+ * @author Shaad Bangi
+ * @version 1.1
+ * @since 2024
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        String url = "jdbc:postgresql://localhost:5432/practice";
+        String username = "postgres";
+        String password = "Shaad#98";
+        System.out.println( "Hello World!" );
+        try{
+            Connection connection = DriverManager.getConnection(url, username, password);
+            System.out.println("Connection is Successful!");
+            connection.close();
+        }catch(Exception e)
+        {
+            System.out.println(e);
+        }
+    }
+}
